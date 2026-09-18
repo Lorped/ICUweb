42dfa8a66555acb87fb945f374b1063cc5a5334d
lorped <lorped@gmail.com>
2026-09-18 13:46:04 +0200

    backend

---
+++ php/cancellaoggetto.php
@@ -0,0 +1,63 @@
+<?php
+
+//http://stackoverflow.com/questions/18382740/cors-not-working-php
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+  header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+  header('Access-Control-Allow-Credentials: true');
+  header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
+
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+    header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+  exit(0);
+}
+
+require_once __DIR__ . '/db2.inc.php'; //MYSQL//
+
+
+$postdata = file_get_contents("php://input");
+$request = json_decode($postdata);
+
+$IDoggetto = $request -> IDoggetto;
+
+
+
+
+if ( isset($postdata) && $IDoggetto != ""  ) {
+
+  $MySql = "DELETE FROM cond_oggetti WHERE IDoggetto = $IDoggetto";
+  $Result = mysqli_query($db, $MySql);
+
+  $MySql = "DELETE FROM oggetti WHERE IDoggetto = $IDoggetto";
+  $Result = mysqli_query($db, $MySql);
+
+  $MySql = "DELETE FROM paired WHERE IDoggetto1 = $IDoggetto OR IDoggetto2 = $IDoggetto";
+  $Result = mysqli_query($db, $MySql);
+
+  $MySql = "DELETE FROM logscanogg WHERE IDoggetto = $IDoggetto";
+  $Result = mysqli_query($db, $MySql);
+
+  $MySql = "DELETE FROM logscanfull WHERE IDoggetto = $IDoggetto";
+  $Result = mysqli_query($db, $MySql);
+
+
+      header("HTTP/1.1 200 OK");
+
+      $out = "OK";
+      $out = json_encode ($out, JSON_UNESCAPED_UNICODE);
+      echo $out;
+
+
+
+} else {
+	header("HTTP/1.1 401 Unauthorized");
+}
+
+
+?>
